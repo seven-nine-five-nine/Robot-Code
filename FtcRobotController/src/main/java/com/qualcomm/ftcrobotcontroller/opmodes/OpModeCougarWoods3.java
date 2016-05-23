2a1494bd8c5caf31c420a1c11a3770145b4447f4
Joseph Holdom <josephholdom@gmail.com>
2016-05-23 16:00:21 -0400

    Because Ian told me to

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OpModeCougarWoods3.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OpModeCougarWoods3.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.util.Range;
 
 /**
- * Created by Mary on 1/8/16, edited on 5/2/16.
+ * Created by Mary on 1/8/16, edited on 5/23/16.
  */
 public class OpModeCougarWoods3 extends OpMode{
 
